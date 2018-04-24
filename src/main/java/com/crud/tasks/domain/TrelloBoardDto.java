fa30a98d6def4b528112308ce1978c3c33af4ab8
JMP27 <marcin.ptaszny@gmail.com>
2018-04-24 22:06:02 +0200

    Trello

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,24 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+//@AllArgsConstructor
+//@Getter
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+
+    public TrelloBoardDto( String name, String id ) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
