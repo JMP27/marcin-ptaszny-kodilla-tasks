37696de21ff5dd3dace66f40206addb8de90d611
JMP27 <marcin.ptaszny@gmail.com>
2018-04-29 16:57:43 +0200

    JAR and PostgreSQL fixed

--- src/main/java/com/crud/tasks/domain/Trello.java
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @AllArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
+@CrossOrigin("*")
 public class Trello {
     @JsonProperty("board")
     private int board;
