23fa19b60a9c6d7093ea4f57367e5f14c45d4673
JMP27 <marcin.ptaszny@gmail.com>
2018-05-06 08:39:35 +0200

    21.1

--- src/test/java/com/crud/tasks/facade/TrelloFacadeTestSuite.java
+++ src/test/java/com/crud/tasks/facade/TrelloFacadeTestSuite.java
@@ -2,59 +2,73 @@ package com.crud.tasks.facade;
 
 import com.crud.tasks.domain.*;
 import com.crud.tasks.mapper.TrelloMapper;
-import com.crud.tasks.service.TrelloService;
-import com.crud.tasks.trello.facade.TrelloFacade;
-import com.crud.tasks.trello.validator.TrelloValidator;
-import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@RunWith(MockitoJUnitRunner.class)
+import static org.junit.Assert.assertEquals;
+
 public class TrelloFacadeTestSuite {
-    @InjectMocks
-    private TrelloFacade trelloFacade;
+    private TrelloMapper trelloMapper = new TrelloMapper();
 
-    @Mock
-    private TrelloValidator trelloValidator;
+     @Test
+     public void tstMapToBoard() throws Exception {
+    // Given
+    List<TrelloListDto> trelloListDtosStub = new ArrayList<>();
+    trelloListDtosStub.add(new TrelloListDto("1", "test list dtos", false));
+    List<TrelloBoardDto> trelloBoardDtosStub = new ArrayList<>();
+    trelloBoardDtosStub.add(new TrelloBoardDto("1", "test board dtos", trelloListDtosStub));
 
-    @Mock
-    private TrelloMapper trelloMapper;
-
-    @Mock
-    private TrelloService trelloService;
-
-    @Test
-    public void shouldFetchTrelloBoards() {
-    //Given
-    List<TrelloBoardDto> fetchedBoards = trelloFacade.fetchTrelloBoards();
-
-    //When
-    trelloFacade.fetchTrelloBoards();
+    // When
+    List<TrelloBoard> fetchedTrelloBoards = trelloMapper.mapToBoard(trelloBoardDtosStub);
 
     //Then
-
-    Assert.assertEquals(0, fetchedBoards.size());
-    System.out.println("the number of fetched boards is: " + fetchedBoards.size());
+    assertEquals(1,fetchedTrelloBoards.size());
+    assertEquals("test board dtos", fetchedTrelloBoards.get(0).getName());
+    assertEquals("test list dtos", fetchedTrelloBoards.get(0).getLists().get(0).getName());
     }
 
     @Test
-    public void shouldCreateCard() {
-    //Given
-    TrelloCardDto card = new TrelloCardDto("card1", "new", "pos", "12");
+    public void testMapToBoardDtos() throws Exception {
+    // Given
+    List<TrelloList> trelloListsStub = new ArrayList<>();
+    trelloListsStub.add(new TrelloList("1", "test lists", false));
+    List<TrelloBoard> trelloBoardsStub = new ArrayList<>();
+    trelloBoardsStub.add(new TrelloBoard("1", "test boards", trelloListsStub));
 
-    //When
-    trelloFacade.createCard(card);
+    // When
+    List<TrelloBoardDto> fetchedTrelloBoardDtos = trelloMapper.mapToBoardsDto(trelloBoardsStub);
 
     //Then
-    Assert.assertEquals("card1", card.getName());
-    System.out.println("the name is: " + card.getName());
+    assertEquals(1,fetchedTrelloBoardDtos.size());
+    assertEquals("test boards", fetchedTrelloBoardDtos.get(0).getName());
+    assertEquals("test lists", fetchedTrelloBoardDtos.get(0).getLists().get(0).getName());
+   }
+
+    @Test
+    public void testMapToCardDto() throws Exception {
+    // Given
+    TrelloCard trelloCard = new TrelloCard("test card", "stub card", "pos", "testId");
+
+    // When
+    TrelloCardDto mappedTrelloCardDto = trelloMapper.mapToCardDto(trelloCard);
+
+    // Then
+    assertEquals("testId", mappedTrelloCardDto.getListId());
     }
 
+    @Test
+    public void testMapToCard() throws Exception {
+        // Given
+        TrelloCardDto trelloCardDto = new TrelloCardDto("test card", "stub card", "pos", "testId");
+
+        // When
+        TrelloCard mappedTrelloCard = trelloMapper.mapToCard(trelloCardDto);
+
+        // Then
+        assertEquals("testId", mappedTrelloCard.getListId());
+    }
 }
 
 
